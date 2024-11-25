10a3c9c7b24bd0736540b0cd52df4248720da851
Radosław Mocarski <mocarski@student.agh.edu.pl>
2024-11-25 12:55:37 +0100

    komentarz

--- oolab/src/main/java/agh/ics/oop/model/util/IncorrectPositionException.java
+++ oolab/src/main/java/agh/ics/oop/model/util/IncorrectPositionException.java
@@ -4,6 +4,6 @@ import agh.ics.oop.model.Vector2d;
 
 public class IncorrectPositionException extends Exception{
     public IncorrectPositionException(Vector2d position) {
-        super("Position " + position + " is not correct.");
+        super("Position " + position + " is not correct."); // Jak dla mnie jest to czytelniejsze od formatted
     }
 }
