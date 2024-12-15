c6655ce81f6a65719abc82830cfa1aaf85821ec8
Radosław Mocarski <mocarski@student.agh.edu.pl>
2024-12-15 01:31:24 +0100

    Update SimulationPresenter.java

--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -1,7 +1,28 @@
 package agh.ics.oop.presenter;
 
+import agh.ics.oop.SimulationApp;
 import agh.ics.oop.model.WorldMap;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SimulationPresenter {
-    WorldMap map;
+    private WorldMap map;
+
+    private void setWorldMap(WorldMap map){
+        this.map = map;
+    }
+
+    private void initializeSimulation(){
+        SimulationApp simulationApp = new SimulationApp();
+        try {
+            simulationApp.start(new Stage());
+        } catch (IOException ignored) {
+
+        }
+    }
+
+    private void drawMap(){
+
+    }
 }
