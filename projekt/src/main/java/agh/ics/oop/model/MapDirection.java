a828cf76636e5b9ffa8075f93f88d8fb3f11a740
tzapart <tomasz.t.zapart@gmail.com>
2024-12-22 19:11:57 +0100

    Create MapDirection
    Create MapDirection

---
+++ projekt/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -0,0 +1,40 @@
+package agh.ics.oop.model;
+
+public enum MapDirection {
+    N(new Vector2d(0, 1)),
+    NE(new Vector2d(1, 1)),
+    E(new Vector2d(1, 0)),
+    SE(new Vector2d(1,-1)),
+    S(new Vector2d(0, -1)),
+    SW(new Vector2d(-1, -1)),
+    W(new Vector2d(-1, 0)),
+    NW(new Vector2d(-1, 1));
+
+    private final Vector2d diretctionToVector;
+
+    MapDirection(Vector2d vector) {
+        diretctionToVector = vector;
+    }
+
+    @Override
+    public String toString() {
+        return switch (this){
+            case N -> "Północ";
+            case NE -> "Północny wschód";
+            case E -> "Wschód";
+            case SE -> "Południowy wschód";
+            case S -> "Południe";
+            case SW -> "Południowy zachód";
+            case W -> "Zachód";
+            case NW -> "Północny zachód";
+        };
+    }
+
+    public MapDirection change(int rotate){
+        return MapDirection.values()[(this.ordinal() + rotate) % 7];
+    }
+
+    public Vector2d toUnitVector(){
+        return diretctionToVector;
+    }
+}
