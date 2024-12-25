e8b7010bc1cf28856b2dbff9b802bea868714a34
Radosław Mocarski <mocarski@student.agh.edu.pl>
2024-12-26 00:31:10 +0100

    komentarz EnergyLoss

--- projekt/src/main/java/agh/ics/oop/model/EnergyLoss.java
+++ projekt/src/main/java/agh/ics/oop/model/EnergyLoss.java
@@ -1,5 +1,5 @@
 package agh.ics.oop.model;
 
 public interface EnergyLoss {
-    int howManyEnergyToWalk(Animal animal);
+    int howManyEnergyToWalk(Animal animal); // zwierzę zamiast vectora zwiększa elastyczność klasom implementującym. Z tego obiektu można odczytać vector, ale inne klasy mogłyby np. odczytać wagę zwierzęcia gdyby chciało itp. Z punktu wydajnościowego różnica pomiędzy vectorem a animalem nie istnieje, bo w obu przypadkach przechowywana jest tylko referencja
 }
